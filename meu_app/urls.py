3ba49ad83ee01627403b373171e543db71d56c55
Mihaael <hugodiego.898@gmail.com>
2025-04-11 23:02:21 -0300

    Adicionando path de login e logout em urls.py

--- meu_app/urls.py
+++ meu_app/urls.py
@@ -1,12 +1,11 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
-from .views import home, cadastro, processar_cadastro, login_usuario
+from .views import home, cadastro, processar_cadastro, login_usuario, logout_usuario
 
 urlpatterns = [
     path('', home, name='home'),  
     path('cadastro/', cadastro, name='cadastro'),
     path('processar_cadastro/', processar_cadastro, name="processar_cadastro"),
     path("login/", login_usuario, name="login"),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    
+    path('logout/', logout_usuario, name='logout'),
 ]
