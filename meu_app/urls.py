744afbbe29f49f54ba919761f78f220ff16f2d2d
LukazinDev <lucasdevctt01@gmail.com>
2025-04-20 17:13:25 -0300

    opdate de urls

--- meu_app/urls.py
+++ meu_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, cadastro, processar_cadastro, login_usuario, logout_usuario
+from .views import home, cadastro, processar_cadastro, login_usuario, logout_usuario, lista_alunos
 from . import views
 
 urlpatterns = [
@@ -8,6 +8,6 @@ urlpatterns = [
     path('processar_cadastro/', processar_cadastro, name="processar_cadastro"),
     path("login/", login_usuario, name="login"),
     path('logout/', logout_usuario, name='logout'),
-    path('alunos/', views.lista_alunos, name='lista_alunos'),
+     path('alunos/', lista_alunos, name='lista_alunos'),
     path('formulario/', views.formulario, name='formulario'),  
 ]
