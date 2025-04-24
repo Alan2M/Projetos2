006fd6e3050538aef0d388c3e2c3fb4fc682cc18
Mihaael <hugodiego.898@gmail.com>
2025-04-24 18:12:52 -0300

    Criando urls da pagina formulario_sucesso.html, estilizando formulario_sucesso.html, views para criar e validar score de vulnerabilidade do formulario, criando admin.py para implementação de mini dashboard com informações de cada usuario cadastrado na tela de admin

--- meu_app/models.py
+++ meu_app/models.py
@@ -2,11 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Perfil(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="perfil")  
+    TIPOS = (
+        ('aluno', 'Aluno'),
+        ('professor', 'Professor'),
+    )
+
+    tipo = models.CharField(max_length=10, choices=[('aluno', 'Aluno'), ('professor', 'Professor')])
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="perfil")
+    nome = models.CharField(max_length=100)  
     cpf = models.CharField(max_length=14, unique=True, blank=True, null=True)  
+    celular = models.CharField(max_length=15, null=True, blank=True)
+    data_nascimento = models.DateField()
+    tipo = models.CharField(max_length=10, choices=TIPOS)
+    score_vulnerabilidade = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.user.username
+        return f"{self.nome} ({self.tipo})"
+
 
 class Aluno(models.Model):
     nome = models.CharField(max_length=100)
@@ -16,3 +29,19 @@ class Aluno(models.Model):
 
     def __str__(self):
         return self.nome
+    
+class FormularioMarcacao(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    pessoas_moram = models.CharField(max_length=50)
+    casa = models.CharField(max_length=50)
+    localizacao = models.CharField(max_length=50)
+    escolaridade_pai = models.CharField(max_length=50)
+    escolaridade_mae = models.CharField(max_length=50)
+    renda_familiar = models.CharField(max_length=50)
+    renda_mensal = models.CharField(max_length=50)
+    trabalha = models.CharField(max_length=10)
+    score = models.IntegerField(default=0)
+    data_envio = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.usuario.username} - Score: {self.score}'
