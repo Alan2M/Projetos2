b6adeb37b1d4e82aad8ef0a70a644aa5c7603a73
JoaoGuilherme <jgbof@cesar.school>
2025-05-05 18:37:46 -0400

    Organiza rotas por seção e adiciona views do painel do gestor

--- meu_app/urls.py
+++ meu_app/urls.py
@@ -1,14 +1,23 @@
 from django.urls import path
-from .views import home, cadastro, processar_cadastro, login_usuario, logout_usuario, lista_alunos
 from . import views
 
 urlpatterns = [
-    path('', home, name='home'),  
-    path('cadastro/', cadastro, name='cadastro'),
-    path('processar_cadastro/', processar_cadastro, name="processar_cadastro"),
-    path("login/", login_usuario, name="login"),
-    path('logout/', logout_usuario, name='logout'),
-    path('alunos/', lista_alunos, name='lista_alunos'),
+    path('', views.home, name='home'),  
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('processar_cadastro/', views.processar_cadastro, name="processar_cadastro"),
+    path('login/', views.login_usuario, name="login"),
+    path('logout/', views.logout_usuario, name='logout'),
+
+    # Formulário de vulnerabilidade
     path("formulario/", views.formulario_view, name="formulario"),
     path('formulario/sucesso/', views.formulario_sucesso, name='formulario_sucesso'),
+
+    # Rotas do gestor
+    path('gestor/', views.painel_gestor, name='painel_gestor'),
+    path('gestor/inicio/', views.inicio_gestor, name='inicio_gestor'),
+    path('gestor/alunos/', views.lista_alunos, name='lista_alunos'),
+    path('gestor/calendario/', views.calendario_entrevistas, name='calendario_entrevistas'),
+
+    # Cadastro de alunos
+    path('alunos/cadastrar/', views.cadastrar_aluno, name='cadastrar_aluno'),
 ]
