46564cc7c3adaa6b6a195ec4b6065ea8aa44eaf6
JoaoGuilherme <jgbof@cesar.school>
2025-05-05 18:36:37 -0400

    Melhorias no admin: adiciona gerenciamento de Aluno e FormularioMarcacao

--- meu_app/admin.py
+++ meu_app/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
-from .models import Perfil
+from .models import Perfil, Aluno, FormularioMarcacao
+
 @admin.register(Perfil)
 class PerfilAdmin(admin.ModelAdmin):
-    list_display = ('user', 'cpf', 'celular', 'score_vulnerabilidade')
-    search_fields = ('user__username', 'cpf', 'celular')
-    list_filter = ('score_vulnerabilidade',)
+    list_display = ('user', 'nome', 'cpf', 'celular', 'score_vulnerabilidade')
+    search_fields = ('user__username', 'nome', 'cpf', 'celular')
+    list_filter = ('tipo', 'score_vulnerabilidade')
     ordering = ('-score_vulnerabilidade',)
 
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'curso_escolhido', 'data_inscricao', 'data_entrevista')
+    search_fields = ('nome', 'email', 'serie')
+    list_filter = ('serie', 'data_entrevista')
+    ordering = ('-data_inscricao',)
+
+    def curso_escolhido(self, obj):
+        return obj.serie
+    curso_escolhido.short_description = "Curso escolhido"
+
+@admin.register(FormularioMarcacao)
+class FormularioMarcacaoAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'score', 'data_envio')
+    search_fields = ('usuario__username',)
+    list_filter = ('score', 'data_envio')
+    ordering = ('-data_envio',)
