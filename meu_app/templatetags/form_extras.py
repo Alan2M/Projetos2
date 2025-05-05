9a67fb6241c1695aa60c50bac78f8761690002a8
JoaoGuilherme <jgbof@cesar.school>
2025-05-05 18:40:47 -0400

    adiciona filtro get_item para acesso a dicionários em templates

--- meu_app/templatetags/form_extras.py
+++ meu_app/templatetags/form_extras.py
@@ -5,3 +5,12 @@ register = template.Library()
 @register.filter(name='add_class')
 def add_class(field, css_class):
     return field.as_widget(attrs={'class': css_class})
+
+
+from django import template
+
+register = template.Library()
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key, [])
